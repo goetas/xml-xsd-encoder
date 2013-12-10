95f4bd72b337b6b225e8650ce2f568e2752b42fe
Asmir Mustafic <asmir@lignano.it>
2013-12-10 11:28:45 +0100

    cs fix

--- lib/Goetas/XmlXsdEncoder/AbstractEncoder.php
+++ lib/Goetas/XmlXsdEncoder/AbstractEncoder.php
@@ -1,80 +1,84 @@
 <?php
 namespace Goetas\XmlXsdEncoder;
 
+abstract class AbstractEncoder
+{
+    private static $refCacheProp = array();
+    private static $refCacheObj = array();
 
-use Doctrine\Common\Util\Inflector;
+    protected static function getValueFrom($variabile, $index)
+    {
+        if (is_array($variabile) && isset($variabile[$index])) {
+            return $variabile[$index];
+        } elseif (is_object($variabile)) {
+            $index = self::camelize($index);
 
-abstract class AbstractEncoder {
+            return self::objectExtract($variabile, $index)->getValue($variabile);
+        }
+    }
+    protected static function tryGetValueFrom($variabile, $index)
+    {
+        try {
+            return self::getValueFrom($variabile, $index);
+        } catch (\ReflectionException $e) {
+            return null;
+        }
+    }
+    protected static function setValueTo(&$variabile, $index, $value)
+    {
+        if (is_array($variabile)) {
+            $variabile[$index] = $value;
+        } elseif ($variabile instanceof \stdClass) {
+            $variabile->$index =  $value;
+        } elseif (is_object($variabile)) {
+            $index = self::camelize($index);
+            self::objectExtract($variabile, $index)->setValue($variabile, $value);
+        }
+    }
+    protected static function addValueTo(&$variabile, $value)
+    {
+        if (is_array($variabile) || $variabile instanceof \ArrayAccess) {
+            $variabile[] = $value;
+        }
+    }
+    protected static function objectExtract($obj, $name)
+    {
+        $c = get_class($obj);
 
-	private static $refCacheProp = array();
-	private static $refCacheObj = array();
+        if (!isset(self::$refCacheProp[$c][$name])) {
 
-	protected static function getValueFrom($variabile, $index){
-		if(is_array($variabile) && isset($variabile[$index])){
-			return $variabile[$index];
-		}elseif(is_object($variabile)){
-			$index = self::camelize($index);
-			return self::objectExtract($variabile, $index)->getValue($variabile);
-		}
-	}
-	protected static function tryGetValueFrom($variabile, $index){
-		try {
-			return self::getValueFrom($variabile, $index);
-		} catch (\ReflectionException $e) {
-			return null;
-		}
-	}
-	protected static function setValueTo(&$variabile, $index, $value){
-		if(is_array($variabile)){
-			$variabile[$index] = $value;
-		}elseif($variabile instanceof \stdClass){
-			$variabile->$index =  $value;
-		}elseif(is_object($variabile)){
-			$index = self::camelize($index);
-			self::objectExtract($variabile, $index)->setValue($variabile, $value);
-		}
-	}
-	protected static function addValueTo(&$variabile, $value){
-		if(is_array($variabile) || $variabile instanceof \ArrayAccess){
-			$variabile[] = $value;
-		}
-	}
-	protected static function objectExtract($obj, $name) {
-		$c = get_class($obj);
+            if (!isset(self::$refCacheObj[$c])) {
+                self::$refCacheObj[$c] = new \ReflectionObject($obj);
+            }
+            try {
+                self::$refCacheProp[$c][$name] =  self::$refCacheObj[$c]->getProperty($name);
+                self::$refCacheProp[$c][$name]->setAccessible(true);
+            } catch (\ReflectionException $e) {
+                throw $e;
+            }
+        }
 
-		if(!isset(self::$refCacheProp[$c][$name])){
+        return self::$refCacheProp[$c][$name];
+    }
 
-			if(!isset(self::$refCacheObj[$c])){
-				self::$refCacheObj[$c] = new \ReflectionObject($obj);
-			}
-			try {
-				self::$refCacheProp[$c][$name] =  self::$refCacheObj[$c]->getProperty($name);
-				self::$refCacheProp[$c][$name]->setAccessible(true);
-			} catch (\ReflectionException $e) {
-				throw $e;
-			}
-		}
-		return self::$refCacheProp[$c][$name];
-	}
+    public static function classify($word)
+    {
+        return str_replace(" ", "", ucwords(strtr($word, "_-", "  ")));
+    }
 
+    /**
+     * Camelize a word. This uses the classify() method and turns the first character to lowercase
+     *
+     * @param  string $word
+     * @return string $word
+     */
+    public static function camelize($word)
+    {
+        if ($word=="__value") {
+            return $word;
+        }
 
-	public static function classify($word)
-	{
-		return str_replace(" ", "", ucwords(strtr($word, "_-", "  ")));
-	}
-
-	/**
-	 * Camelize a word. This uses the classify() method and turns the first character to lowercase
-	 *
-	 * @param string $word
-	 * @return string $word
-	 */
-	public static function camelize($word)
-	{
-		if($word=="__value"){
-			return $word;
-		}
-		return lcfirst(self::classify($word));
-	}
+        return lcfirst(self::classify($word));
+    }
 
 }
--- lib/Goetas/XmlXsdEncoder/LitteralEncoder.php
+++ lib/Goetas/XmlXsdEncoder/LitteralEncoder.php
@@ -7,131 +7,129 @@ use goetas\webservices\exceptions\ConversionNotFoundException;
 
 use goetas\webservices\bindings\soap\MessageComposer;
 
-use goetas\xml\xsd\SimpleContent;
-
 use goetas\xml\xsd\ComplexType;
 
 use goetas\xml\xsd\SimpleType;
 
 use goetas\xml\xsd\AbstractComplexType;
 
-use goetas\xml\xsd\SchemaContainer;
-
 use goetas\xml\xsd\Type;
 
-use goetas\xml\XMLDomElement;
+class LitteralEncoder extends AbstractEncoder implements EncoderInterface
+{
+    /**
+     *
+     * @var \goetas\webservices\bindings\soap\MessageComposer
+     */
+    protected $composer;
+    const NS_XSI = 'http://www.w3.org/2001/XMLSchema-instance';
 
-class LitteralEncoder extends AbstractEncoder implements EncoderInterface {
-	/**
-	 *
-	 * @var \goetas\webservices\bindings\soap\MessageComposer
-	 */
-	protected $composer;
-	const NS_XSI = 'http://www.w3.org/2001/XMLSchema-instance';
+    public function __construct(MessageComposer $composer)
+    {
+        $this->composer = $composer;
+    }
+    public function supports(Type $type)
+    {
+        return ($type instanceof ComplexType);
+    }
+    public function encode($variable, \DOMNode $node, Type $type)
+    {
+        if ($type instanceof AbstractComplexType) {
 
-	public function __construct(MessageComposer $composer) {
-		$this->composer = $composer;
-	}
-	public function supports(Type $type){
-		return ($type instanceof ComplexType);
-	}
-	public function encode($variable, \DOMNode $node, Type $type) {
+            foreach ($type->getAttributes() as $attribute) {
 
-		if($type instanceof AbstractComplexType){
+                $val  = self::getValueFrom($variable, $attribute->getName());
 
-			foreach ($type->getAttributes() as $attribute){
+                if ($val!==null) {
+                    if ($attribute->getQualification()=="qualified") {
+                        $attributeNode = $node->ownerDocument->ocreateAttributeNS($attribute->getNs(), $node->getPrefixFor($attribute->getNs()).":".$attribute->getName());
+                        $node->setAttributeNodeNs($attributeNode);
+                    } else {
+                        $attributeNode = $node->ownerDocument->createAttribute($attribute->getName());
+                        $node->setAttributeNode($attributeNode);
+                    }
+                    $this->convertSimplePhpXml($val, $attributeNode, $attribute->getType());
 
-				$val  = self::getValueFrom($variable, $attribute->getName());
+                } elseif ($attribute->isRequred()) {
+                    throw new \Exception("Type $type, attributo ".$attribute." non deve essere vuoto");
+                }
+            }
 
-				if($val!==null){
-					if($attribute->getQualification()=="qualified"){
-						$attributeNode = $node->ownerDocument->ocreateAttributeNS($attribute->getNs(), $node->getPrefixFor($attribute->getNs()).":".$attribute->getName());
-						$node->setAttributeNodeNs($attributeNode);
-					}else{
-						$attributeNode = $node->ownerDocument->createAttribute($attribute->getName());
-						$node->setAttributeNode($attributeNode);
-					}
-					$this->convertSimplePhpXml($val, $attributeNode, $attribute->getType());
+            if ($type->getBase()) {
+                $this->encode($variable, $node, $type->getBase());
+            }
 
-				}elseif($attribute->isRequred()){
-					throw new \Exception("Type $type, attributo ".$attribute." non deve essere vuoto");
-				}
-			}
+            if ($type instanceof ComplexType) {
 
-			if($type->getBase()){
-				$this->encode($variable, $node, $type->getBase());
-			}
+                foreach ($type->getElements() as $element) {
 
-			if($type instanceof ComplexType){
+                    $elementQualified = $element->getQualification()=="qualified";
+                    $newType = $element->getType();
 
-				foreach ($type->getElements() as $element){
+                    if($element->getMax()>1 &&
+                            ($val = self::tryGetValueFrom($variable, $element->getName()))
+                            && (is_array($val) || $val instanceof \Traversable)
 
-					$elementQualified = $element->getQualification()=="qualified";
-					$newType = $element->getType();
+                    ){
+                        foreach ($val as $nval) {
+                            if ($elementQualified) {
+                                $newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
+                            } else {
+                                $newNode = $node->addChild($element->getName());
+                            }
+                            $this->encode($nval, $newNode, $newType);
+                        }
+                    } elseif ($element->getMax()>1 && (is_array($variable) || $variable instanceof \Traversable)) {
 
-					if($element->getMax()>1 &&
-							($val = self::tryGetValueFrom($variable, $element->getName()))
-							&& (is_array($val) || $val instanceof \Traversable)
+                        foreach ($variable as $nval) {
+                            if ($elementQualified) {
+                                $newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
+                            } else {
+                                $newNode = $node->addChild($element->getName());
+                            }
+                            $this->encode($nval, $newNode, $newType);
+                        }
 
-					){
-						foreach ($val as $nval){
-							if($elementQualified){
-								$newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
-							}else{
-								$newNode = $node->addChild($element->getName());
-							}
-							$this->encode($nval, $newNode, $newType);
-						}
-					}elseif($element->getMax()>1 && (is_array($variable) || $variable instanceof \Traversable)){
+                    } else {
 
-						foreach ($variable as $nval){
-							if($elementQualified){
-								$newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
-							}else{
-								$newNode = $node->addChild($element->getName());
-							}
-							$this->encode($nval, $newNode, $newType);
-						}
+                        $val  = self::getValueFrom($variable, $element->getName());
 
-					}else{
+                        if ($val!==null || $element->isNillable()) {
 
-						$val  = self::getValueFrom($variable, $element->getName());
+                            if ($elementQualified) {
+                                $newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
+                            } else {
+                                $newNode = $node->addChild($element->getName());
+                            }
+                            if ($val===null) {
+                                $newNode->setAttributeNS(self::NS_XSI, $newNode->getPrefixFor(self::NS_XSI).":nil", "true");
+                            } else {
+                                $this->encode($val, $newNode, $newType);
+                            }
 
-						if($val!==null || $element->isNillable()){
+                        } elseif ($element->getMin()>0) {
+                            throw new \Exception($element." non deve essere vuoto");
+                        }
+                    }
+                }
+            }
+        }
+        if ($type instanceof SimpleType) {
+            $this->convertSimplePhpXml($variable, $node, $type);
+        }
+    }
 
-							if($elementQualified){
-								$newNode = $node->addPrefixedChild($element->getNs(), $element->getName());
-							}else{
-								$newNode = $node->addChild($element->getName());
-							}
-							if($val===null){
-								$newNode->setAttributeNS(self::NS_XSI, $newNode->getPrefixFor(self::NS_XSI).":nil", "true");
-							}else{
-								$this->encode($val, $newNode, $newType);
-							}
-
-						}elseif($element->getMin()>0){
-							throw new \Exception($element." non deve essere vuoto");
-						}
-					}
-				}
-			}
-		}
-		if($type instanceof SimpleType){
-			$this->convertSimplePhpXml($variable, $node, $type);
-		}
-	}
-
-	protected function convertSimplePhpXml($value, $node, SimpleType $xsd) {
-		try {
-			$this->composer->encode($value, $node, $xsd);
-		} catch (ConversionNotFoundException $e) {
-			$base = $xsd->getBase();
-			if ($base){
-				$this->composer->encode($value, $node, $base);
-			}else{
-				throw $e;
-			}
-		}
-	}
+    protected function convertSimplePhpXml($value, $node, SimpleType $xsd)
+    {
+        try {
+            $this->composer->encode($value, $node, $xsd);
+        } catch (ConversionNotFoundException $e) {
+            $base = $xsd->getBase();
+            if ($base) {
+                $this->composer->encode($value, $node, $base);
+            } else {
+                throw $e;
+            }
+        }
+    }
 }
